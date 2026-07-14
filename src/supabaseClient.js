eda1edb30aa7477a4b0d39222310ff7f07aae868
RiCE-Desu <rice12415@gmail.com>
2026-07-14 22:08:03 +0700

    Stop tracking .env file

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Ganti string di bawah ini dengan URL dan Key dari dashboard Supabase lu!
-const supabaseUrl = 'https://rxuncgnuoejycyeescbp.supabase.co/';
-const supabaseAnonKey = 'sb_publishable_TLfZLtLv5W-GV4Dysr_Flw_bu2pnl4L';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
